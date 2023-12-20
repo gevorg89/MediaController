c02c06f9df2000a77696ab53ae93a7886b0f0148
Gevorg <gevorg89@gmail.com>
2023-12-20 16:33:31 +0300

    init

--- MediaController/mobile/src/main/java/com/gevorg89/mobile/MainActivity.kt
+++ MediaController/mobile/src/main/java/com/gevorg89/mobile/MainActivity.kt
@@ -7,12 +7,9 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gevorg89.mediacommon.MainViewModel
 import com.gevorg89.mobile.ui.theme.MediaControllerTheme
 
