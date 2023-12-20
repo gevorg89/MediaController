05fcab6578011484d0d05c88644e16d84070ff03
Gevorg <gevorg89@gmail.com>
2023-12-20 15:57:33 +0300

    init

--- MediaController/common/build.gradle.kts
+++ MediaController/common/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation("androidx.wear.compose:compose-foundation:1.1.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    testImplementation("org.testng:testng:6.9.6")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
